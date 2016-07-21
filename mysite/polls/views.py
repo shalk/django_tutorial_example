79320a526e7582e5498e9223cedd8029043db521
shalk <shalk@aliyun.com>
2016-07-21 17:38:26 +0800

    modify view add loader.render with context

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render
+from django.template import loader
 
 # Create your views here.
 
 from django.http import HttpResponse
+from .models import Question
 
-def index(self):
-    return HttpResponse("Hello, world. You're at the polls index.")
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    #output = ','.join([ q.question_text for q in latest_question_list ])
+    #return HttpResponse(output)
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
 
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
